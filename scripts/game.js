30126b5f7d66d2a0eac1ccb16481b6b7c9ebbd6f
Pipos <marnei.machado@gmail.com>
2020-07-22 18:16:07 -0300

    fineshed

--- scripts/game.js
+++ scripts/game.js
@@ -1,19 +1,52 @@
 //iniciar variaveis
 let board = ['','','','','','','','','']
 let PlayerTime = 0
-let symbols = ['o', 'x']
+let gameOver = false;
 
+let symbols = ['o', 'x']
+let winStates = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
 
 function handleMove(position){
+
+    if(gameOver){
+        return
+    }
+
     if(board[position] == ''){
         board[position] = symbols[PlayerTime]
 
-        if (PlayerTime == 0){
-            PlayerTime = 1
-        }else{
-            PlayerTime = 0
+        gameOver = isWin()
+        
+        if(gameOver == false){
+            PlayerTime = (PlayerTime == 0) ? 1 : 0
         }
     }
+    return gameOver
+}
 
+function isWin(){
 
+    for (let i = 0; i < winStates.length; i++){
+        let = seq = winStates[i]
+
+        let pos1 = seq[0]
+        let pos2 = seq[1]
+        let pos3 = seq[2]
+        
+        if(board[pos1] == board[pos2] && 
+            board[pos1] == board[pos3] && 
+            board[pos1] != ''){
+            return true
+        }
+    }
+    return false
 }
--- scripts/interface.js
+++ scripts/interface.js
@@ -13,10 +13,22 @@ function handleClick(event){
     let square = event.target
     let position = square.id
 
-    handleMove(position)
-    updateSqaures()
-
+    if (handleMove(position)){
+        
+        setTimeout(() => {
+            alert('O jogo acabou - O vencer foi ' + PlayerTime)
+        }, 10)
+    }
+    updateSqaure(position)
 }
+
+function updateSqaure(position){
+    let square = document.getElementById(position.toString())
+    let symbol = board[position]
+    square.innerHTML = `<div class='${symbol}'></div>`
+}
+
+
 
 function updateSqaures(){
 
@@ -31,5 +43,4 @@ function updateSqaures(){
             square.innerHTML = `<div class='${symbol}'></div>` 
         }
     })
-
 }
